d4e4b1ed172228056524acaff1aa90fe80c22fd3
lamzuong <lamdavid821@gmail.com>
2022-10-27 20:44:30 +0700

    commit

--- ThucHanh_Tuan7/bai4/App.js
+++ ThucHanh_Tuan7/bai4/App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useRef, useState } from "react";
+import { Animated, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const splashscreen = useRef(new Animated.Value(0)).current;
+  const txtWelcome = useRef(new Animated.Value(0)).current;
+  const txtTacgia = useRef(new Animated.Value(0)).current;
+  const [hideScreen, setHideScreen] = useState(false);
+  const [hideTextWelcome, setHideTextWelcome] = useState(false);
+  const [hideTextTacGia, setHideTextTacGia] = useState(true);
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(splashscreen, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(splashscreen, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(txtWelcome, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(txtWelcome, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(txtTacgia, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+    ]).start();
+    setTimeout(() => {
+      setHideTextWelcome(false);
+      setHideScreen(true);
+    }, 2100);
+    setTimeout(() => {
+      setHideTextWelcome(true);
+      setHideTextTacGia(false);
+    }, 4200);
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Animated.View style={styles.container}>
+      {hideScreen ? null : (
+        <Animated.Image
+          style={[{ width: 400, height: 400 }, { opacity: splashscreen }]}
+          source={require("./assets/Zola.png")}
+        />
+      )}
+      {hideTextWelcome ? null : (
+        <Animated.View style={{ opacity: txtWelcome }}>
+          <Text style={{ fontSize: 20 }}>Welcome to Splash Screen</Text>
+        </Animated.View>
+      )}
+      {hideTextTacGia ? null : (
+        <Animated.View style={{ opacity: txtTacgia }}>
+          <Text style={{ fontSize: 20 }}>Author: Vương Ánh Lâm</Text>
+        </Animated.View>
+      )}
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
